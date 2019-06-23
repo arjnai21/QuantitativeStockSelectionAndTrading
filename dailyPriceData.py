26dba5157d38364d5f70a05071ddadeccd5fcc55
arjnai21 <arjnai21@bergen.org>
2019-06-23 17:05:07 -0400

    actually gets price

--- dailyPriceData.py
+++ dailyPriceData.py
@@ -1,9 +1,12 @@
-from pandas_datareader import data
-import matplotlib.pyplot as plt
-import pandas as pd
+from iexfinance.stocks import Stock, get_historical_data
+from datetime import datetime
+# token="sk_957a78ce716949109cf36f83b9cc3fd2"
 
+start = datetime(2017, 1, 1)
+end = datetime(2018, 1, 1)
 
-
+apple = Stock("AAPL", token="sk_957a78ce716949109cf36f83b9cc3fd2")
+print(apple.get_price())
 
 
 
