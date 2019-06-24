b732976a55bba9c720837f16da76c414d1b86eda
arjnai21 <arjnai21@bergen.org>
2019-06-23 20:02:57 -0400

    cleanup

--- dailyPriceData.py
+++ dailyPriceData.py
@@ -1,16 +1,13 @@
-from iexfinance.stocks import Stock, get_historical_data
+from iexfinance.stocks import get_historical_data
 from datetime import datetime
-import csv
 
-import matplotlib.pyplot as plt
 # token="sk_957a78ce716949109cf36f83b9cc3fd2"
 
 start = datetime(2019, 1, 1)
-end = datetime(2019, 6, 23)
+end = datetime.today()
 
 
 with open("aapl_test.csv", mode="w") as file:
-    file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     apple = get_historical_data("AAPL", start, end, token="sk_957a78ce716949109cf36f83b9cc3fd2", output_format = 'pandas')
     apple.to_csv(path_or_buf=file)
 
