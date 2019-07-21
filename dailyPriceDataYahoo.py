fa3a8ac558680dc9151c5bf8975eb7423fe77bae
arjnai21 <arjun@nairsnet.com>
2019-07-20 20:05:02 -0400

    comments

--- dailyPriceDataYahoo.py
+++ dailyPriceDataYahoo.py
@@ -3,41 +3,49 @@ import csv
 import datetime
 import os
 
+# stock tickers that have no data
 not_working = ["ZAZZT", "ZBZZT", "ZXYZ.A", "BRPM.U", "CBX",  "FPAC.W", "GIG.R", "GIG.W", "GIX.U", "GRAF.U", "GSAH.W",
                "IAA.V", "IBO", "IGZ", "LGC.U", "LGC.W", "MFAC.W", "MOSC.U", "NTEST.C", "PVT.U", "PVT.W", "RMG.W",
                "SHLL.U", "SPAQ.U", "TRNE.W", "TRNO", "VST.A", "ZTEST"]
+
+# stock tickers that have a '.' but who's YahooFinancials call requires a '-' instead
 not_right = ["AGM.A", "AKO.A", "AKO.B", "BF.A", "BF.B", "BH.A", "BIO.B", "BRK.A", "BRK.B", "BWL.A", "CBS.A", "CRD.A",
              "CRD.B", "CWEN.A", "GEF.B", "GTN.A", "HEI.A", "HVT.A", "JW.A", "JW.B", "MKC.V", "MOG.A", "MOG.B", "STZ.B",
              "TAP.A", "WSO.B"]
 
+
 start_date = "2019-01-01"
 end_date = datetime.date.today().__str__()
 
-x = 0
 for ticker in open("tickers.txt"):
     ticker = ticker.strip('\n')
     if ticker in not_right:
         ticker = ticker.replace('.', '-')
-    print(ticker + " " + str(x))
-    x += 1
-    if ticker in not_working or os.path.isfile("data/" + ticker + ".csv") or ticker[:5] == "ATEST":
+
+    # if ticker has no data. All tickers that begin with 'ATEST' have no data
+    if ticker in not_working or ticker[:5] == "ATEST":
         print("skipped")
         continue
+
+    # collect historical data for ticker
     financials = YahooFinancials(ticker)
     data = financials.get_historical_price_data(start_date, end_date, "daily")
     prices = data[ticker]["prices"]
-    ordering = ['high', 'low', 'open', 'close', 'volume', 'adjclose']
+
+    # header of csv
+    ordering = ['High', 'Low', 'Open', 'Close', 'Volume', 'Adjclose']
+
+    # rearrange values with different keys
     for i in range(len(prices)):
         formatted_dict = {'Date': prices[i]['formatted_date']}
         for j in ordering:
-            formatted_dict[ticker + "." + j.title()] = prices[i][j]
+            formatted_dict[ticker + "." + j] = prices[i][j.lower()]
         prices[i] = formatted_dict
-        del formatted_dict
 
     keys = prices[0].keys()
 
+    # write prices dictionary to csv file
     with open('data/' + ticker + '.csv', 'w', newline='') as file:
         dict_writer = csv.DictWriter(file, keys)
         dict_writer.writeheader()
-
         dict_writer.writerows(prices)
