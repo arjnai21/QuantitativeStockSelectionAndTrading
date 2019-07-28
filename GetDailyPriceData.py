3afa067b77b56666e388f49bc17d754cc86e10cf
arjnai21 <arjun@nairsnet.com>
2019-07-28 13:48:24 -0400

    comments

---
+++ GetDailyPriceData.py
@@ -0,0 +1,14 @@
+from CollectPriceData import collectData
+from GetStockTickers import getTickers
+
+"""
+This file completes the entire task
+"""
+
+def main():
+    getTickers()
+    collectData()
+
+
+main()
+
