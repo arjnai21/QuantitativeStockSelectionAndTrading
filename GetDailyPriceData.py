5952a7a61bcb8f02aaf1eb3645dc7e47f8482048
arjnai21 <arjun@nairsnet.com>
2019-07-28 14:09:37 -0400

    comments

--- GetDailyPriceData.py
+++ GetDailyPriceData.py
@@ -4,6 +4,10 @@ from GetStockTickers import getTickers
 """
 This file completes the entire task
 
+IMPORTANT: It is necessary to install the yahoofinancials API, which can be done simply with the command
+
+pip install yahoofinancials
+
 Creates a folder in the working directory titled 'data', and writes all the .csv files for each ticker to it
 
 """
