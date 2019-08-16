afbc1df75c3829385bd669249906164a0d8d63e0
arjnai21 <arjun@nairsnet.com>
2019-08-16 16:51:41 -0400

    comments

--- FBMovingAverage.py
+++ FBMovingAverage.py
@@ -4,10 +4,12 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 
 ticker = "FB"
-df = quandl.get('WIKI/' + ticker)
+df = quandl.get('WIKI/' + ticker, api_key="T2K2v57vDVL9Wwx_ia3c")
 df["MA5"] = df['Close'].rolling(5).mean()
 df["MA10"] = df['Close'].rolling(10).mean()
-with PdfPages(r'C:\Users\arjun\PycharmProjects\QuantitativeStockSelection\FBMovingAverage.pdf') as export_pdf:
+
+
+with PdfPages(r'C:\Users\arjun\PycharmProjects\QuantitativeStockSelectionAndTrading\FBMovingAverage.pdf') as export_pdf:
     df['Close'].plot()
     df['MA5'].plot()
     df["MA10"].plot()
