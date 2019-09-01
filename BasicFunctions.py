5876a08a0c728d8d26823652848da0e408b28fa0
arjnai21 <arjnai21@bergen.org>
2019-09-01 02:03:03 -0400

    cleanup

--- BasicFunctions.py
+++ BasicFunctions.py
@@ -1,11 +1,18 @@
-def yr_vol(val, daily_scale=252):
+# def yr_vol(val, daily_scale=252):
+#
+# def yr_rtn(val, daily_scale=252):
+#
+# def max_drawdn(val):
+#
+# def ma_trade(price, ma_fast=5, ma_slow=10):
+#
+# def price2invest(price, i_hold, cash=-1):
+#     if cash == -1:
+#         cash = price[1]
 
-def yr_rtn(val, daily_scale=252):
+def read_stock_into_list(filename):
+    with open(filename) as f:
+        lines = [float(i) for i in f.readlines()]
+    return lines
 
-def max_drawdn(val):
-
-def ma_trade(price, ma_fast=5, ma_slow=10):
-
-def price2invest(price, i_hold, cash=-1):
-    if cash == -1:
-        cash = price[1]
+print(read_stock_into_list("StockX.txt"))
