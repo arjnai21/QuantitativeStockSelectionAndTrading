2652edfd7688358a94bd1848b7e055cc2e3c39a6
arjnai21 <arjnai21@bergen.org>
2019-09-04 01:57:11 -0400

    cleanup

--- BasicFunctions.py
+++ BasicFunctions.py
@@ -1,9 +1,32 @@
-# def yr_vol(val, daily_scale=252):
+import statistics
+import math
+def yr_vol(val, daily_scale=252):
+    daily_percent_change = [((val[i] - val[i-1]) / val[i-1]) for i in range(1, len(val))]
+    return statistics.pstdev(daily_percent_change) * math.sqrt(daily_scale)
 #
 # def yr_rtn(val, daily_scale=252):
 #
-# def max_drawdn(val):
-#
+def max_drawdn(val):
+    peaks = [0]
+    peak_val = val[0]
+    for i in range(1, len(val)-1):
+        if val[i] > peak_val and val[i] > val[i+1]:
+            peak_val = val[i]
+            peaks.append(i)
+    peaks.append(len(val) + 1) # slicing purposes
+    max_drawdn = 0
+    for i in range(len(peaks)-1):
+        testing = val[peaks[i]:peaks[i+1]]
+        drawdn = val[peaks[i]] - min(val[peaks[i]:peaks[i+1]]) # search from peak to peak for minum
+        if drawdn > max_drawdn:
+            max_drawdn = drawdn
+            print(max_drawdn)
+
+    return max_drawdn # CURRENTLY PRICE DROP NOT PERCENTAGE
+
+
+
+
 def ma_trade(price, ma_fast=5, ma_slow=10):
     ret = []
     for i in range(ma_slow):
@@ -41,5 +64,8 @@ def read_stock_into_list(filename):
     return lines
 
 data = read_stock_into_list("StockX.txt")
+print(min(data))
 trade = ma_trade(data)
-print(price2invest(data, trade, 1000))
+print(price2invest(data, trade, 46570))
+print(yr_vol(data))
+print(max_drawdn(data))
---
+++ test.py
@@ -0,0 +1,8 @@
+print(int("32\n"))
+print(open("StockX.txt").readlines())
+print(sum([1, 2, 3]))
+x = [1, 2, 3, 4, 5, 6]
+print((x[1:3]))
+
+x.insert(10, "q")
+print(x)
