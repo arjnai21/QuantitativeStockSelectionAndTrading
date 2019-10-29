45d3fec2e1fde86bf544a656e1f0cc73453fcca3
arjnai21 <arjun@nairsnet.com>
2019-10-29 08:58:09 -0400

    comments

--- BasicFunctions.py
+++ BasicFunctions.py
@@ -4,25 +4,27 @@ import math
 
 
 def yr_vol(val, daily_scale=252):
-    daily_percent_change = [((val[i] - val[i-1]) / val[i-1]) for i in range(1, len(val))]
+    daily_percent_change = [((val[i] - val[i - 1]) / val[i - 1]) for i in range(1, len(val))]
     return statistics.pstdev(daily_percent_change) * math.sqrt(daily_scale)
+
 
 def yr_rtn(val, daily_scale=252):
     cum_rtn = (val[-1] - val[0]) / val[0]
-    return (1+cum_rtn) ** (365/len(val)) - 1
+    return (1 + cum_rtn) ** (365 / len(val)) - 1
 
 
 def max_drawdn(val):
     peaks = [0]
     peak_val = val[0]
-    for i in range(1, len(val)-1):
-        if val[i] > peak_val and val[i] > val[i+1]:
+    for i in range(1, len(val) - 1):
+        if val[i] > peak_val and val[i] > val[i + 1]:
             peak_val = val[i]
             peaks.append(i)
     peaks.append(len(val) + 1)  # slicing purposes
     max_drawdown = 0
-    for i in range(len(peaks)-1):
-        drawdn = (val[peaks[i]] - min(val[peaks[i]:peaks[i+1]])) / val[peaks[i]] # search from peak to peak for minimum
+    for i in range(len(peaks) - 1):
+        drawdn = (val[peaks[i]] - min(val[peaks[i]:peaks[i + 1]])) / val[
+            peaks[i]]  # search from peak to peak for minimum
         if drawdn > max_drawdown:
             max_drawdown = drawdn
 
@@ -34,8 +36,8 @@ def ma_trade(price, ma_fast=5, ma_slow=10):
     for i in range(ma_slow):
         ret.append(0)
     for i in range(ma_slow, len(price)):
-        fast_avg = sum(price[i-ma_fast:i]) / ma_fast
-        slow_avg = sum(price[i-ma_slow:i]) / ma_slow
+        fast_avg = sum(price[i - ma_fast:i]) / ma_fast
+        slow_avg = sum(price[i - ma_slow:i]) / ma_slow
         if fast_avg > slow_avg:
             ret.append(1)
         else:
@@ -49,10 +51,10 @@ def price_2_invest(price, i_hold, cash=None):
     values = []
     num_shares = 0
     for i in range(len(price)):
-        if i_hold[i] == 0 and i != 0 and i_hold[i-1] == 1:
+        if i_hold[i] == 0 and i != 0 and i_hold[i - 1] == 1:
             cash = num_shares * price[i]
             num_shares = 0
-        if i_hold[i] == 1 and (i == 0 or i_hold[i-1] == 0):
+        if i_hold[i] == 1 and (i == 0 or i_hold[i - 1] == 0):
             num_shares = cash / price[i]
             cash = 0
         values.append(cash + num_shares * price[i])
@@ -65,10 +67,11 @@ def read_stock_into_list(filename):
         lines = [float(i) for i in f.readlines()]
     return lines
 
+
 def get_moving_avg(price, freq):
-    ma = [None for i in range(freq)]
+    ma = [None for j in range(freq)]
     for i in range(freq, len(price)):
-        ma.append(sum(price[i-freq:i]) / freq)
+        ma.append(sum(price[i - freq:i]) / freq)
     return ma
 
 
@@ -89,7 +92,13 @@ def graph_price_2_invest(price, ma_fast=5, ma_slow=60, cash=None):
 
 data = read_stock_into_list("StockX.txt")
 # randomly generated list of 1s and 0s
-hold = [0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0]
+hold = [0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+        1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0,
+        1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0,
+        1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1,
+        0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0,
+        0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1,
+        1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0]
 print(yr_vol(data))
 print(yr_rtn(data))
 print(max_drawdn(data))
@@ -97,6 +106,3 @@ print(ma_trade(data))
 print(price_2_invest(data, hold))
 print(get_moving_avg(data, 60))
 graph_price_2_invest(data)
-
-
-
